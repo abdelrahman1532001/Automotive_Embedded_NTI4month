682b1eef33645af1083e8d077bff64883336b715
Abdelrahman <abdelrahman1532001@gmail.com>
2023-08-03 19:53:44 +0300

    Push the right file

--- Atmega32_arch_interfacing/HAL/KEYPAD_Driver/KPD_Prog.c
+++ Atmega32_arch_interfacing/HAL/KEYPAD_Driver/KPD_Prog.c
@@ -5,44 +5,38 @@
  *      Author: ENG-Abdelrahman
  */
 
-#include "../../LIB/stdtypes.h"
+#include "../../LIB/Std_Types.h"
 
-#include "../../MCAL/AT32_DIO_Driver/AT32_DIO_interface.h"
+#include "../../MCAL/DIO_Driver/DIO_Interface.h"
 
-#include "KPD_config.h"
-#include "KPD_interface.h"
-#include "KPD_private.h"
 
-u8 KPD_u8KeyMap[KPD_NUM_OF_ROWS][KPD_NUM_OF_COLS] = KPD_KEY_MAP;
-u8 KPD_u8Row_Pins[KPD_NUM_OF_ROWS] = KPD_ROW_PINS;
-u8 KPD_u8Col_Pins[KPD_NUM_OF_COLS] = KPD_COL_PINS;
+#include "KPD_Cfg.h"
+#include "KPD_Interface.h"
+#include "KPD_Private.h"
+
+u8 KPD_u8KeyMap[KPD_NUM_OF_ROWS][KPD_NUM_OF_COLS] = KPD_KEYS_MAP;
+u8 KPD_u8Row_Pins[KPD_NUM_OF_ROWS] = KPD_ROWS_PINS;
+u8 KPD_u8Col_Pins[KPD_NUM_OF_COLS] = KPD_COLS_PINS;
 /*****************************************************************************************************
- *  Description ::: function to initiate the keypad                                                  *
+ *  Description ::: function to initiate the keypad pins                                                *
  *___________________________________________________________________________________________________*
  *        input ::: none                                                                             *
  *___________________________________________________________________________________________________*
  *       output ::: none                                                                             *
  *                                                                                                   *
  *****************************************************************************************************/
-void KPD_voidInit(void)
+void KPD_vidInit(void)
 {
 	//variables
 	u8 Local_u8Pin;
 
-	//loop on rows and set it as output
+	//Loop On Rows' Pins And Make Them HIGH
 	for(Local_u8Pin = 0; Local_u8Pin < KPD_NUM_OF_ROWS; Local_u8Pin++)
 	{
-		AT32_DIO_u8PinInit(KPD_ROW_PORT, KPD_u8Row_Pins[Local_u8Pin], output); //set as output
-		AT32_DIO_u8SetPinValue(KPD_ROW_PORT, KPD_u8Row_Pins[Local_u8Pin], high); //set high as default
-	}//end for
+		DIO_vidPin_Write(KPD_u8Row_Pins[Local_u8Pin], HIGH); //Set High As Default
+	}//End For
 
-	//loop on columns and set it as input and active pull up
-	for(Local_u8Pin = 0; Local_u8Pin < KPD_NUM_OF_COLS; Local_u8Pin++)
-	{
-		AT32_DIO_u8PinInit(KPD_COL_PORT, KPD_u8Col_Pins[Local_u8Pin], input_pullup); //set as output
-	}//end for
-
-}//end function
+}
 /*****************************************************************************************************
  *  Description ::: function to read from the keypad                                                 *
  *___________________________________________________________________________________________________*
@@ -51,28 +45,32 @@ void KPD_voidInit(void)
  *       output ::: none                                                                             *
  *                                                                                                   *
  *****************************************************************************************************/
-u8 KPD_voidReadKey(void)
+u8 KPD_u8ReadKey(void)
 {
 	//Variables
-	u8 Loca_u8RowPin, Loca_u8ColPin, Local_u8ColRead, Local_u8PressedKey = KPD_NOT_PRESSED;
+	u8 Loca_u8RowPin; //counter to loop on rows
+	u8 Loca_u8ColPin; //counter to loop on columns
+	DIO_PIN_VOLT_e Local_u8ColRead; //variable to save pin value
+	u8 Local_u8PressedKey = KPD_NOT_PRESSED; //variable to save pressed key
 	//loop on rows and set one row to high
 	for(Loca_u8RowPin = 0; Loca_u8RowPin < KPD_NUM_OF_ROWS; Loca_u8RowPin++)
 	{
 		//set one row as low
-		AT32_DIO_u8SetPinValue(KPD_ROW_PORT, KPD_u8Row_Pins[Loca_u8RowPin], low);
+		DIO_vidPin_Write(KPD_u8Row_Pins[Loca_u8RowPin], LOW);
 		//loop on columns and check
 		for(Loca_u8ColPin = 0; Loca_u8ColPin < KPD_NUM_OF_COLS; Loca_u8ColPin++)
 		{
 			//read each column to find the pressed key
-			AT32_DIO_u8GetPinValue(&Local_u8ColRead, KPD_COL_PORT, KPD_u8Col_Pins[Loca_u8ColPin]);
-			if(Local_u8ColRead == 0) //this happen when a key be pressed
+			Local_u8ColRead = DIO_Pin_Read(KPD_u8Col_Pins[Loca_u8ColPin]);
+			if(Local_u8ColRead == LOW) //this happen when a key be pressed
 			{
 				Local_u8PressedKey = KPD_u8KeyMap[Loca_u8RowPin][Loca_u8ColPin]; //save the pressed key
-				AT32_DIO_u8SetPinValue(KPD_ROW_PORT, KPD_u8Row_Pins[Loca_u8RowPin], high); //make the row high again
-				break; //get out after find the key
+				while(DIO_Pin_Read(KPD_u8Col_Pins[Loca_u8ColPin]) == LOW); //make sure the key is released
+				DIO_vidPin_Write(KPD_u8Row_Pins[Loca_u8RowPin], HIGH); //make the row high again
+				return Local_u8PressedKey; //exit with the pressed key
 			}//end if
 		}//end columns loop
-		AT32_DIO_u8SetPinValue(KPD_ROW_PORT, KPD_u8Row_Pins[Loca_u8RowPin], high); //make the row high again
+		DIO_vidPin_Write(KPD_u8Row_Pins[Loca_u8RowPin], HIGH); //make the row high again
 	}//end rows loop
 
 	//if press none key return with no press
